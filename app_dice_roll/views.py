4a6098f5c99e085ded589339706724d8d8b1e2a3
Ale171104 <valero7rinke@gmail.com>
2024-01-11 13:39:48 -0300

    test

--- app_dice_roll/views.py
+++ app_dice_roll/views.py
@@ -4,6 +4,63 @@ from django.http import HttpResponse
 import random
 
 
+class Dice:
+
+
+    def __init__(self, sides):
+        self.sides = sides
+        self.increment = 0
+        self.result = 0
+    
+
+
+        match sides:
+            case 4:
+                self.increment = 1081080000
+
+            case 6:
+                self.increment = 772200000
+
+            case 8:
+                self.increment = 600600000
+
+            case 10:
+                self.increment = 491400000
+
+            case 12:
+                self.increment = 415800000  
+
+            case 20:
+                self.increment = 257400000              
+
+
+
+    def roll(self):
+        random_value = random.randint(1, 5405400000)
+        divisor = 0
+        
+
+
+        for side in range(1, self.sides + 1):
+            divisor = divisor + self.increment
+
+            if random_value <= divisor:
+                self.result = int(side)                             
+                print(self.result)
+                break
+
+
+    def get_result_previosly_rolled(self):
+        return self.result
+
+
+
+d4 = Dice(4)
+d6 = Dice(6)
+d8 = Dice(8)
+d10 = Dice(10)
+d12 = Dice(12)
+d20 = Dice(20)
 
 
 
@@ -16,66 +73,7 @@ def home(request):
 
 
 def result(request):
-
-    class Dice:
-
-
-        def __init__(self, sides):
-            self.sides = sides
-            self.increment = 0
-            self.result = 0
-        
-
-
-            match sides:
-                case 4:
-                    self.increment = 1081080000
-
-                case 6:
-                    self.increment = 772200000
-
-                case 8:
-                    self.increment = 600600000
-
-                case 10:
-                    self.increment = 491400000
-
-                case 12:
-                    self.increment = 415800000  
-
-                case 20:
-                    self.increment = 257400000              
-
-
-
-        def roll(self):
-            random_value = random.randint(1, 5405400000)
-            divisor = 0
-            
-
-
-            for side in range(1, self.sides + 1):
-                divisor = divisor + self.increment
-
-                if random_value <= divisor:
-                    self.result = int(side)                             
-                    print(self.result)
-                    break
-
-
-        def get_result_previosly_rolled(self):
-            return self.result
- 
-
-
-    d4 = Dice(4)
-    d6 = Dice(6)
-    d8 = Dice(8)
-    d10 = Dice(10)
-    d12 = Dice(12)
-    d20 = Dice(20)
     
-
     if request.method == 'POST':
         option = request.POST.get('dice')
         dice_type = "d0"
